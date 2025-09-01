2849db8d91bdf99665305bfcb5a510c57c17b72e
Erik Grahn <erikgrahn13@gmail.com>
2025-09-01 22:40:05 +0200

    initial commit of react

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <img
+        src="https://raw.githubusercontent.com/erikgrahn13/audio/main/resources/assets/logo_transparent.png"
+        alt="Logo"
+        width="50%"
+      />
+      <h1>To be announced soon...</h1>
+    </>
+  )
+}
+
+export default App
